16a92fadd11fbc945e9062fca04a7f0ed72a1a76
Christian Paredes <chriscip02@gmail.com>
2022-11-07 20:44:16 -0300

    sigo adelante

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,1 @@
-const abrirCarrito = document.getElementById('botonCarrito');
-
-const cerrarCarrito =document.getElementById('carritoCerrar');
-
-const modal = document.querySelector('.modal-contenedor');
-
-const modalDesactivado = document.querySelector('#carritoCerrar');
-
-abrirCarrito.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.add('modalActive');
-});
-
-carritoCerrar.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.remove('modalActive');
-});
 
